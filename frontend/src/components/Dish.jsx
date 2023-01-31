6a7a5335ce30f059d7edd393aec799252f5798b3
Filip Gumula <filgum0326@gmail.com>
2023-01-31 23:16:42 +0100

    Changes to BasketMenu, implementation of orders, empty Dish component

---
+++ frontend/src/components/Dish.jsx
@@ -0,0 +1,3 @@
+const Dish = () => {};
+
+export default Dish;
--- frontend/src/components/global/BasketMenu.jsx
+++ frontend/src/components/global/BasketMenu.jsx
@@ -1,15 +1,19 @@
-import { Box, Typography, IconButton } from "@mui/material";
+import { Box, Typography, IconButton, Avatar } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import CloseIcon from "@mui/icons-material/Close";
-import { fontSize } from "@mui/system";
 import { useDispatch, useSelector } from "react-redux";
-
 import { setIsBasketVisible } from "../../state";
+import { decreaseCount, increaseCount } from "../../state";
 
 const BasketMenu = () => {
   const dispatch = useDispatch();
   const isBasketVisible = useSelector((state) => state.basket.isBasketVisible);
+  const basket = useSelector((state) => state.basket.basket);
+
+  const total = basket.reduce((price, item) => {
+    return (price += item.attributes.price * item.count);
+  }, 0);
 
   return (
     <Box
@@ -27,7 +31,7 @@ const BasketMenu = () => {
         display="flex"
         flexDirection="column"
         position="fixed"
-        width="max(40%, 325px)"
+        width="max(30%, 325px)"
         height="100%"
         top="0"
         right="0"
@@ -35,16 +39,16 @@ const BasketMenu = () => {
         padding="16px"
         backgroundColor="rgba(0,0,0, 0.95)"
       >
-        {/* TOP  */}
+        {/* TOP */}
         <Box
           width="100%"
-          height="50px"
           display="flex"
           justifyContent="space-between"
           alignItems="center"
+          borderBottom="1px solid white"
         >
           <Typography variant="h3" color="white">
-            Shopping Basket
+            Your Shopping Basket
           </Typography>
           <IconButton
             caria-label="close"
@@ -55,6 +59,71 @@ const BasketMenu = () => {
           >
             <CloseIcon sx={{ color: "white" }} />
           </IconButton>
+        </Box>
+        {/* TOP END */}
+
+        {/* INDIVIDUAL DISH ORDER */}
+        <Box>
+          {basket.map((dish) => {
+            return (
+              // wrapper for individual order
+              <Box
+                key={`${dish.id}_${dish.attributes.name}`}
+                sx={{
+                  border: "2px solid red",
+                  p: 2,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                {/* Image of product */}
+                <Box>
+                  <Avatar
+                    alt={`${dish.attributes.name}`}
+                    sx={{ width: 56, height: 56 }}
+                    src={`http://localhost:1337${dish?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                  />
+                </Box>
+                {/* Name of product */}
+                <Box>
+                  <Typography variant="h5" fontWeight="bold" color="white">
+                    {dish.attributes.name}
+                  </Typography>
+                </Box>
+                <Box>
+                  <IconButton
+                    onClick={() => {
+                      dispatch(decreaseCount({}));
+                    }}
+                  >
+                    <RemoveIcon />
+                  </IconButton>
+                </Box>
+                <Box>
+                  <Typography variant="h5" color="white" fontWeight="bold">
+                    {dish.count}
+                  </Typography>
+                </Box>
+                <Box>
+                  <IconButton
+                    onClick={() => {
+                      dispatch(increaseCount({}));
+                    }}
+                  >
+                    <AddIcon />
+                  </IconButton>
+                </Box>
+              </Box>
+            );
+          })}
+        </Box>
+        {/* BOTTOM - USER ACTIONS */}
+
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <Typography variant="h5" color="white" mt={6} fontWeight="bold">
+            Total price: {`${total}`}
+          </Typography>
         </Box>
       </Box>
     </Box>
